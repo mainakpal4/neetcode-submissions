120c687d892ce723e3b38191f470402926f30635
Mainak Pal <palmainakmax@gmail.com>
2026-09-10 13:19:39 +0530

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        Map<Character,Integer> hm=new HashMap<>();
+        int l=0,maxP=0,res=0;
+
+        for(int r=0;r<s.length();r++){
+            hm.put(s.charAt(r),hm.getOrDefault(s.charAt(r),0)+1);
+            maxP=   Math.max(maxP,hm.get(s.charAt(r)));
+
+            while((r-l+1)-maxP>k){
+                hm.put(s.charAt(l),hm.get(s.charAt(l))-1);
+                l++;
+            }
+            res=Math.max(res,r-l+1);
+            
+        }
+
+        return res;
+    }
+}
